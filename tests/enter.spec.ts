2f245c6ff82173b64a034e09ed3a2744cf2dce93
patri-coder <patrickmukadi2017@gmail.com>
2022-11-26 18:22:53 +0900

    second commit

---
+++ tests/enter.spec.ts
@@ -0,0 +1,52 @@
+import { expect, Expect, test } from "@playwright/test";
+import AccessPage from "../Pages/Access";
+import AddBasketPage from "../Pages/AddBasket";
+import ApplyPage from "../Pages/Apply";
+import DashboardPage from "../Pages/Dashboard";
+import UrlLink from "../Pages/Url";
+
+
+const Email = "patrickm@roboco6000-op.org";
+const Password = "password";
+test.describe("Application Form", function(){
+    test("Apply for first time formality", async ({page,baseURL})=>{     
+        const link = new UrlLink(page)  
+        await link.UrlPage1(`${baseURL}route=account/register`)
+        // await page.goto(`${baseURL}route=account/register`); 
+        const apply = new ApplyPage(page);
+        await apply.typeFirstName("Patrick");         
+        await apply.typeRenterName("roboco-op");
+        await apply.typeEmail(Email);
+        await apply.typePhone("81079737434")
+        await apply.typePassword(Password);      
+        await apply.typeRenterPassword(Password);
+        await apply.NotSubscribed(); 
+        await apply.AgreeCheck(); 
+        await apply.Validate();
+        await apply.Time();
+    })
+    test("Access In ", async({page, baseURL})=>{     
+
+        const link = new UrlLink(page);
+        const time = new ApplyPage(page)
+        await link.UrlPage2(`${baseURL}route=account/login`) 
+        const access = new AccessPage(page);
+        await access.TypeEmail(Email);  
+        await access.TypePassword(Password);
+        await access.LoginButton() 
+        // expect (await page.title()).toBe("My Account")    
+        await time.Time()
+        
+    })
+    test("Access in Side",async({page,baseURL})=>{       
+        const url = new UrlLink(page)
+        const access = new AccessPage(page)
+        const add = new AddBasketPage(page)
+        const dashboard = new DashboardPage(page)
+        await url.UrlPage2(`${baseURL}route=account/login`) 
+        await access.Login(Email, Password);
+        await dashboard.goToMenu()   
+        await add.add()    
+    })
+
+})
