749d9f8964084c97756aad3db9a80524e08b0319
patri-coder <patrickmukadi2017@gmail.com>
2023-01-14 00:35:35 +0900

    fix enter.spec.ts file

--- tests/enter.spec.ts
+++ tests/enter.spec.ts
@@ -5,7 +5,7 @@ import ApplyPage from "../Pages/Apply";
 import DashboardPage from "../Pages/Dashboard";
 import UrlLink from "../Pages/Url";
 
-const Email = "patrick@counts-oprs.org";
+const Email = "patrick@counts-oprsnet.org";
 const Password = "password";
 test.describe("Application Form", function () {
   test("Apply for first time formality", async ({ page, baseURL }) => {
@@ -15,7 +15,6 @@ test.describe("Application Form", function () {
     // await RegisterLink.LinkRegister();
     const apply = new ApplyPage(page);
     await page.goto("https://ecommerce-playground.lambdatest.io/index.php?route=account/register")
-   
     await apply.typeFirstName("Patrick");
     await apply.typeRenterName("roboco-op");
     await apply.typeEmail(Email);
@@ -28,33 +27,33 @@ test.describe("Application Form", function () {
     await apply.Time();
   });
 
-//   test.skip("Access In ", async ({ page, baseURL }) => {
+  test("Access In ", async ({ page, baseURL }) => {
 
-     // await page.goto(`${baseURL}route=account/login`);
-    // const time = new ApplyPage(page);
-    // const access = new AccessPage(page);
-    // const LoginLink = new UrlLink(page)
+     await page.goto(`${baseURL}route=account/login`);
+    const time = new ApplyPage(page);
+    const access = new AccessPage(page);
+    const LoginLink = new UrlLink(page)
 
-    // await LoginLink.LinkLogin()
-    // await access.TypeEmail(Email);
-    // await access.TypePassword(Password);
-    // await access.LoginButton();
-    // expect (await page.title()).toBe("My Account")
-//     await time.Time();
-//   });
-//   test.skip("Access in Side", async ({ page, baseURL }) => {
+    await LoginLink.LinkLogin()
+    await access.TypeEmail(Email);
+    await access.TypePassword(Password);
+    await access.LoginButton();
+    expect (await page.title()).toBe("My Account")
+    await time.Time();
+  });
+  test.only("Access in Side", async ({ page, baseURL }) => {
     
-    // await page.goto(`${baseURL}route=account/login`);
-//     const access = new AccessPage(page);
-//     const add = new AddBasketPage(page);
-//     const dashboard = new DashboardPage(page);
-//     const Link = new UrlLink(page)
+    await page.goto(`${baseURL}route=account/login`);
+    const access = new AccessPage(page);
+    const add = new AddBasketPage(page);
+    const dashboard = new DashboardPage(page);
+    const Link = new UrlLink(page)
 
-//     await Link.LinkLogin();
-//     await access.Login(Email, Password);
-//     await dashboard.goToMenu();
-//     await add.add();
-//     const CartVisible = await add.isVisible();
-//     await expect(CartVisible).toBeVisible();
-//   });
+    await Link.LinkLogin();
+    await access.Login(Email, Password);
+    await dashboard.goToMenu();
+    await add.add();
+    const CartVisible = await add.isVisible();
+    await expect(CartVisible).toBeVisible();
+  });
 });
